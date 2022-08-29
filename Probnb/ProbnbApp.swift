5aa154cfdf6371294b99e6766ccd1e7317a8df68
Dongju Park <pdj95044@gmail.com>
2022-08-29 21:22:13 +0900

    Initial Commit

---
+++ Probnb/ProbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbnbApp.swift
+//  Probnb
+//
+//  Created by Dongju Park on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct ProbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
