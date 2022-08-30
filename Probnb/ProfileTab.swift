9aa60383dc0069634fd60997e6607ac933292f77
Yunsu Kim <101382788+yunssu@users.noreply.github.com>
2022-08-30 12:29:09 +0900

    Add ProfileTab

---
+++ Probnb/ProfileTab.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTab.swift
+//  Probnb
+//
+//  Created by 윤수 on 2022/08/30.
+//
+
+import SwiftUI
+
+struct ProfileTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileTab_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileTab()
+    }
+}
