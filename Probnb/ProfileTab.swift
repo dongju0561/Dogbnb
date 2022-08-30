d22772bcedf7fdf289daf0568a028e11f8990eb7
Dongju Park <pdj95044@gmail.com>
2022-08-30 19:18:20 +0900

    test commit

---
+++ Probnb/ProfileTab.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTab.swift
+//  Probnb
+//
+//  Created by 윤수 on 2022/08/30.
+//
+
+import SwiftUI
+
+struct ProfileTab: View {
+    var body: some View {
+        Text("ProfileTab!!")
+    }
+}
+
+struct ProfileTab_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileTab()
+    }
+}
